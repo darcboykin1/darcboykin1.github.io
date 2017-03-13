c97cfdab48cf966067f31870e5f5758ddec54535
Darc Boykin <darcboykin@gmail.com>
2017-03-13 02:35:10 -0400

    m

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+// $.ajax({
+//         url: 'https://portfolio-917bc.firebaseio.com/',
+//         type: 'GET'
+//     })
+//     .done(function() {
+//         console.log("success");
+//     })
+//     .fail(function() {
+//         console.log("error");
+//     })
+//     .always(function() {
+//         console.log("complete");
+//     });
 
+$(document).ready(function() {
+    console.log("Ready")
+})
