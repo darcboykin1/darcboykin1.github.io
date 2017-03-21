90afd2de60f6388eeb61aff68f2e75d6e0f3359d
Darc Boykin <darcboykin@gmail.com>
2017-03-20 20:19:45 -0400

    add project descriptions

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-// $.ajax({
-//         url: 'https://portfolio-917bc.firebaseio.com/',
-//         type: 'GET'
-//     })
-//     .done(function() {
-//         console.log("success");
-//     })
-//     .fail(function() {
-//         console.log("error");
-//     })
-//     .always(function() {
-//         console.log("complete");
-//     });
-
 $(document).ready(function() {
     console.log("Ready")
 })
