5003f55d6be2fce7ffc9ed3e9ff1b91774e3b172
darcboykin1 <darcboykin@gmail.com>
2017-06-24 20:14:45 -0400

    begin the rework

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("JavaScript is connected");
+
+$(document).ready(function(){
+	console.log("jQuery is connected");
+})
+
+// $(document).ready(function(){
+// 	$(".menuitem").hover(function(){
+		
+// 	})
+// })
