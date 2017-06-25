cc02d32f4806b610a581b88a1b8edbdcfaf0bc3e
darcboykin1 <darcboykin@gmail.com>
2017-06-24 20:26:33 -0400

    add smooth scrolling to the page

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ console.log("JavaScript is connected");
 
 $(document).ready(function(){
 	console.log("jQuery is connected");
-})
+});
+
+$("a").smoothScroll();
 
 // $(document).ready(function(){
 // 	$(".menuitem").hover(function(){
-		
+
 // 	})
 // })
