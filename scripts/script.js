cf9b5b15e864a15c939f3be9491f51c5d231a690
darcboykin1 <darcboykin@gmail.com>
2017-08-16 11:06:35 -0400

    edit the formatting and routing

--- scripts/script.js
+++ scripts/script.js
@@ -24,7 +24,10 @@ axios.get('https://portfolio-917bc.firebaseio.com/projects.json')
     	<div class="project">
     		<h1 class="project_name">${key}</h1>
     		<br>
-    		<p class="project_description">${meat.description}</p>
+    		<div style="width: 405px; background-color: rgba(0,0,0,0);">
+    			<p class="project_description">${meat.description}</p>
+    		</div>
+    		<h3>Skills Used:</h3>
     		<ul class="${key}">
     		</ul>
     		<div class="icon_home">
@@ -41,7 +44,7 @@ axios.get('https://portfolio-917bc.firebaseio.com/projects.json')
 });
 
     window.sr = ScrollReveal({
-    	reset: true
+    	reset: false
     });
 
 	sr.reveal('.project', {duration: 500}, 150);
