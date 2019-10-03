2376136e48d2f58486555b0e023dbc3eaa772fd7
Darc <darcboykin@gmail.com>
2019-10-03 18:53:22 -0400

    updated styling

--- index.js
+++ index.js
@@ -17,25 +17,28 @@ document.addEventListener("DOMContentLoaded", function(){
         let meat = projects[key];
     
         let skills = meat.skills;
-    
-        console.log(skills)
-    
+
         $(".projects").append(`
             <div class="project">
-                <h1 class="project_name">${key}</h1>
+                <h1 class="project_name"><span class="pinklight">${key}</span></h1>
                 <br>
-                <div class="descriptDiv" style="background-color: rgba(0,0,0,0);">
-                    <p class="project_description">${meat.description}</p>
+                <div class="projectFlex">
+                    <img src="${meat.cover}" width="400px">
+                    <br>
+                    <div class="projectInner">
+                        <div class="descriptDiv" style="background-color: rgba(0,0,0,0);">
+                            <p class="project_description">${meat.description}</p>
+                        </div>
+                        <br>
+                        <h3>Skills Used:</h3>
+                        <ul class="${key}">
+                        </ul>
+                        <div class="icon_home">
+                            <a href="${meat.repo}" target="_blank"><img src="https://banner2.cleanpng.com/20180704/uxe/kisspng-github-computer-icons-icon-design-desktop-wallpape-5b3d36142dd125.8636932415307381961877.jpg" title="Check out the repo!" class="linkToRepo"></a>
+                            <a href="${meat.link}" target="_blank"><img src="https://www.hixagency.com/Images/globe-icon.png" title="Try it out!" class="linkToApp"></a>
+                        </div>
+                    </div>
                 </div>
-                <br>
-                <h3>Skills Used:</h3>
-                <ul class="${key}">
-                </ul>
-                <div class="icon_home">
-                    <a href="${meat.repo}" target="_blank"><img src="https://banner2.cleanpng.com/20180704/uxe/kisspng-github-computer-icons-icon-design-desktop-wallpape-5b3d36142dd125.8636932415307381961877.jpg" title="Check out the repo!" class="linkToRepo"></a>
-                    <a href="${meat.link}" target="_blank"><img src="https://www.hixagency.com/Images/globe-icon.png" title="Try it out!" class="linkToApp"></a>
-                </div>
-            </div>
             `
         );
     
@@ -44,7 +47,10 @@ document.addEventListener("DOMContentLoaded", function(){
         };
     });
         $(document).ready(function(){
-            $('.projects').slick();
+            $('.projects').slick({
+                arrows:true,
+                dots: true
+            });
         });
         // window.sr = ScrollReveal();
     
